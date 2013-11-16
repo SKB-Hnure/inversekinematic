203b3e7a028f5deb8f0254950786caf5a5c6f3cf
Diana <diana@diana.(none)>
2013-11-16 17:45:33 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include "three_angles.h"
+#include <unistd.h>  /* Объявления стандартных функций UNIX */
+#include <fcntl.h>   /* Объявления управления файлами */
+#include <errno.h>   /* Объявления кодов ошибок */
+#include <termios.h> /* Объявления управления POSIX-терминалом */
+using namespace std;
+#define Pi 3.1415
+double Qa, Qb, Qc, B, x, y, dZ, x02,y02,y2, L1,L2,L3;
+
+double abs(double a)
+{
+    if (a>=0) return a;
+    else return (-a);
+}
+string to_string(int val) {
+ char buff[32];
+ sprintf(buff,"%d",val);
+ return string(buff);
+}
+
+int sends(char st[], int length)                                         //ОТКРЫТИЕ COM-ПОРТА
+{
+  int fd; /* Файловый дескриптор для порта */
+
+
+  fd = open("/dev/ttyUSB1", O_RDWR | O_NOCTTY | O_NDELAY);
+  if (fd == -1)
+  {
+       perror("open_port: Unable to open /dev/ttyUSB1 - ");
+  }
+  else
+    fcntl(fd, F_SETFL, 0);   // Установить значение флагов описателя, заданное в arg
+int n = write(fd, st, length);
+if (n < 0)
+  fputs("write() of 4 bytes failed!\n", stderr);
+  close(fd);
+  return (fd);
+}
+int main ()
+{
+    int n[3];
+    for(int i=0;i<3;i++)
+ {
+     cout<<"L"<<i+1<<" Servo №: ";
+     cin>>n[i];
+ }
+   cout<<"L1="; cin>>L1;
+   cout<<"L2="; cin>>L2;
+   cout<<"L3="; cin>>L3;
+    	while (x!=-1)
+{
+	cout<<"X="; cin>>x;
+	cout<<"Y="; cin>>y;
+	cout<<"Z="; cin>>dZ;
+	for (int i=0;i<3;i++)
+	cout<<"Q["<<i+1<<"]="<<angles(L1,L2,L3,x,y,dZ)[i]<<" grad\n";
+int Q[3];
+Q[0]=(1856*angles(L1,L2,L3,x,y,dZ)[0])/180+1500;
+Q[1]=(1856*(90-angles(L1,L2,L3,x,y,dZ)[1]))/180+544;
+Q[2]=(1856*(180-angles(L1,L2,L3,x,y,dZ)[2]))/180+544;
+struct Imp{
+    int min;
+    int max;};
+Imp I[3];
+ I[0].min=1300;
+ I[0].max=1700;
+ I[1].min=1180;
+ I[1].max=2340;
+ I[2].min=1650;
+ I[2].max=2330;
+ for (int i=0;i<3;i++)
+ {
+     if (Q[i]<I[i].min) Q[i]=I[i].min;
+     if (Q[i]>I[i].max) Q[i]=I[i].max;
+ }
+string str, str1;
+for (int i=0;i<3;i++)
+{ str.append("#");
+str.append(to_string(n[i]));
+str.append("P");
+str.append(to_string(Q[i]));
+}
+const int d=700;
+str.append("T");
+str.append(to_string(d));
+str.append("\n\r");
+cout<<str;
+char st[50];
+int length=str.length();
+for (int i=0;i<length;i++)
+{
+    st[i]=str[i];
+}
+sends(st,length);
+char getch;
+cin>>getch;
+}
+  return 0;
+}
---
+++ three_angles.h
@@ -0,0 +1,34 @@
+#ifndef THREE_ANGLES_H_INCLUDED
+#define THREE_ANGLES_H_INCLUDED
+
+
+double* angles(double L1, double L2, double L3, double x, double y, double dZ)
+/*
+   L1, L2, L3 - длины звеньев, где L1 поворачивается в горизонтальной плоскости, L2, L3 - в вертикальной
+   x,y - координаты захвата манипулятора
+   dZ - высота
+   возвращает массив из трех углов в градусах
+   [0] - угол между осью Х и L1, c y>0
+   [1] - угол, ограниченный снизу прямой, параллельной плоскости XY, а сверху звеном L2
+   [2] - угол, ограниченный сверху прямой, продолжающей звено L2, в снизу звеном L3
+   */
+{ double *Q = new double[3];
+Q[0]=atan(y/x);
+  y2=sqrt(pow(x,2)+pow(y,2))-L1;
+ B=sqrt(dZ*dZ+pow(y2,2));
+ if (y2>0)
+ Q[1]=Pi/2-asin((L2*L2-L3*L3+B*B)/(2*B*L2))-atan(dZ/y2);
+ else  {
+     if (y2<0)  Q[1]=Pi/2-asin((L2*L2-L3*L3+B*B)/(2*B*L2))-(Pi-abs(atan(dZ/y2)));
+     if (y2==0) Q[1]=Pi/2-asin((L2*L2-L3*L3+B*B)/(2*B*L2))-Pi/2;
+ }
+ Q[2]=Pi/2+asin((L2*L2+L3*L3-B*B)/(2*L2*L3));
+ for (int i=0;i<3;i++)
+ {
+     Q[i]=180*Q[i]/Pi;
+ }
+    return Q;
+}
+
+
+#endif // THREE_ANGLES_H_INCLUDED
